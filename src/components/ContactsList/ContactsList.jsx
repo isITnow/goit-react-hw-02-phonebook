813d2b745e2f5d89f3a4918fb47da1e9a2eee7e3
Roman Serediuk <box4rom@gmail.com>
2022-09-08 22:11:37 +0300

    task completed

---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import s from './ContactsList.module.css';
+import ContactsItem from './ContactsItem';
+
+export default function ContactsList({ contacts, onDelete }) {
+  return (
+    <ul className={s.contacts__list}>
+      {contacts.map(({ name, number, id }) => {
+        return (
+          <ContactsItem
+            key={id}
+            name={name}
+            number={number}
+            id={id}
+            onDelete={onDelete}
+          />
+        );
+      })}
+    </ul>
+  );
+}
+
+ContactsList.propTypes = {
+  onDelete: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+};
